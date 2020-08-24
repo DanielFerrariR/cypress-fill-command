ba975c05946e8919cf0256a5d96d17d34c2e8038
Daniel Ferrari <danielferrari@live.com>
2020-08-24 16:03:17 -0300

    feat(/): adding tsdoc

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,5 +1,11 @@
 declare namespace Cypress {
   interface Chainable {
+    /**
+     * Fills an input or textarea element.
+     * @param value - Text to be filled
+     * @example
+     *    cy.get('#the-element-id').fill('Some text')
+     */
     fill(value: string): Chainable<void>
   }
 }
